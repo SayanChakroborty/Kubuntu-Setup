ba0b06b752b767e129cbb36c7a592bb887d91533
Sayan Chakroborty <sayan@e.email>
2020-12-12 18:48:25 +0530

    Create Pre-Update.sh

---
+++ Pre-Update.sh
@@ -0,0 +1,33 @@
+sudo -s << EOF
+
+apt purge thunderbird snapd -y
+
+sed -i '/\/swapfile/d' /etc/fstab
+
+echo -e "\nvm.swappiness = 0\nvm.vfs_cache_pressure = 1\nvm.dirty_background_bytes = 4194304\nvm.dirty_bytes = 4194304\n" >> /etc/sysctl.d/99-sysctl.conf
+
+sysctl -p
+
+apt install fish linux-tools-common linux-tools-generic linux-tools-$(uname -r)-generic -y
+
+echo -e "# Allow members of group sudo to execute any command\n%sudo ALL=(ALL:ALL) NOPASSWD: ALL\n" > /etc/sudoers.d/00_nopasswd
+
+chmod 0440 /etc/sudoers.d/00_nopasswd
+
+echo -e "[Bypass Polkit for Everything]\nIdentity=unix-group:sudo\nAction=*\nResultActive=yes\n" > /var/lib/polkit-1/localauthority/50-local.d/bypass-polkit.pkla
+
+echo -e "\n\nENTER PASSWORD TO CHANGE ROOT SHELL TO FISH:"
+
+chsh -s /usr/bin/fish
+
+EOF
+
+echo -e "\n\nENTER PASSWORD TO CHANGE USER SHELL TO FISH:"
+
+chsh -s /usr/bin/fish
+
+echo "sudo cpupower frequency-set -g performance" > ~/.config/autostart-scripts/cpupower.sh
+
+echo "krunner --daemon &" > ~/.config/autostart-scripts/krunner.sh
+
+chmod a+x ~/.config/autostart-scripts/*
