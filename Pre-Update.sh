2d184853e930f945b182037c4c3a44a895898bff
Sayan Chakroborty <sayan@e.email>
2020-12-13 21:12:15 +0530

    Update Pre-Update.sh

--- Pre-Update.sh
+++ Pre-Update.sh
@@ -10,7 +10,7 @@ echo -e "\nvm.swappiness = 0\nvm.vfs_cache_pressure = 1\nvm.dirty_background_byt
 
 sysctl -p
 
-apt install -y fish linux-tools-common linux-tools-generic linux-tools-$(uname -r)-generic
+apt install -y linux-tools-common linux-tools-generic
 
 echo -e "# Allow members of group sudo to execute any command\n%sudo ALL=(ALL:ALL) NOPASSWD: ALL\n" > /etc/sudoers.d/00_nopasswd
 
@@ -18,15 +18,7 @@ chmod 0440 /etc/sudoers.d/00_nopasswd
 
 echo -e "[Bypass Polkit for Everything]\nIdentity=unix-group:sudo\nAction=*\nResultActive=yes\n" > /var/lib/polkit-1/localauthority/50-local.d/bypass-polkit.pkla
 
-echo -e "\n\nENTER PASSWORD TO CHANGE ROOT SHELL TO FISH:"
-
-chsh -s /usr/bin/fish
-
 EOF
-
-echo -e "\n\nENTER PASSWORD TO CHANGE USER SHELL TO FISH:"
-
-chsh -s /usr/bin/fish
 
 echo "sudo cpupower frequency-set -g performance" > ~/.config/autostart-scripts/cpupower.sh
 
