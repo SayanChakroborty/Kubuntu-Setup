5f2370888c90b2e16311859d7794b096c9f1bedf
Sayan Chakroborty <sayan@e.email>
2020-12-14 18:41:55 +0530

    Update Post-Update.sh

--- Post-Update.sh
+++ Post-Update.sh
@@ -20,18 +20,8 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
 
 sudo add-apt-repository -y 'deb https://download.onlyoffice.com/repo/debian squeeze main'
 
-sudo add-apt-repository -y ppa:lutris-team/lutris
-
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-
-sudo apt-key add winehq.key
-
-rm ./winehq.key
-
-sudo add-apt-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-
 sudo apt update
 
 sudo apt full-upgrade -y
 
-sudo apt install --install-recommends -y libreoffice-kde dolphin-nextcloud papirus-icon-theme papirus-folders qownnotes persepolis qbittorrent kodi telegram onlyoffice-desktopeditors onlyoffice-documentbuilder libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 winehq-staging lutris gamemode
+sudo apt install --install-recommends -y libreoffice-kde dolphin-nextcloud papirus-icon-theme papirus-folders qownnotes persepolis qbittorrent kodi telegram onlyoffice-desktopeditors onlyoffice-documentbuilder
