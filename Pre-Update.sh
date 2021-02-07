51cea64b0de3b618b0d17c4bf5b15ae1578bba57
Sayan Chakroborty <sayan@e.email>
2021-02-07 20:41:04 +0530

    Update Pre-Update.sh

--- Pre-Update.sh
+++ Pre-Update.sh
@@ -10,8 +10,6 @@ echo -e "\nvm.swappiness = 0\nvm.dirty_background_bytes = 4194304\nvm.dirty_byte
 
 sysctl -p
 
-apt install -y linux-tools-common linux-tools-generic
-
 echo -e "# Allow members of group sudo to execute any command\n%sudo ALL=(ALL:ALL) NOPASSWD: ALL\n" > /etc/sudoers.d/00_nopasswd
 
 chmod 0440 /etc/sudoers.d/00_nopasswd
@@ -19,13 +17,3 @@ chmod 0440 /etc/sudoers.d/00_nopasswd
 echo -e "[Bypass Polkit for Everything]\nIdentity=unix-group:sudo\nAction=*\nResultActive=yes\n" > /var/lib/polkit-1/localauthority/50-local.d/bypass-polkit.pkla
 
 EOF
-
-mkdir -p ~/.config/plasma-workspace/env
-
-echo "/usr/bin/sudo /usr/bin/cpupower frequency-set -g performance" > ~/.config/plasma-workspace/env/cpupower.sh
-
-chmod a+x ~/.config/plasma-workspace/env/*
-
-echo "/usr/bin/sleep 10 && /usr/bin/krunner --daemon" > ~/.config/autostart-scripts/krunner.sh
-
-chmod a+x ~/.config/autostart-scripts/*
