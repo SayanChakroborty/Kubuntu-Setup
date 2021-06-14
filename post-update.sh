5797fb4d74364260b70f0fa8106c4e3b34960fe2
Sayan Chakroborty <sayan@e.email>
2021-06-14 11:26:34 +0530

    Update post-update.sh

--- post-update.sh
+++ post-update.sh
@@ -10,6 +10,10 @@ add-apt-repository -y ppa:papirus/papirus
 
 add-apt-repository -y ppa:team-xbmc/ppa
 
+add-apt-repository -y ppa:persepolis/ppa
+
+add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
 
 add-apt-repository -y 'deb https://download.onlyoffice.com/repo/debian squeeze main'
@@ -18,6 +22,6 @@ apt update
 
 apt full-upgrade -y
 
-apt install --install-recommends -y libreoffice libreoffice-kde dolphin-nextcloud papirus-icon-theme papirus-folders kodi onlyoffice-desktopeditors onlyoffice-documentbuilder
+apt install --install-recommends -y libreoffice libreoffice-kde dolphin-nextcloud papirus-icon-theme papirus-folders kodi persepolis qbittorrent onlyoffice-desktopeditors onlyoffice-documentbuilder
 
 EOF
